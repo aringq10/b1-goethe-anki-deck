528133025d77b63a975305b3226570d62f843d69
aringq <aringas15@gmail.com>
2024-10-11 09:33:32 +0300

    translated sentences from other.csv, made a list of imposter words

--- deepl_api.js
+++ deepl_api.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const fs = require('fs');
 const csv = require('csvtojson');
-const csvFilePath = `${__dirname}/backup/nouns.csv`;
+const csvFilePath = `${__dirname}/data/other.csv`;
 
 // DEEPL
 const deepl = require('deepl-node');
@@ -13,11 +13,13 @@ csv()
 .then(async (jsonObj) => {
     const stream = fs.createWriteStream(`${__dirname}/translated.csv`, {flags:'a'});
     try {
-        for (let i = 929; i < jsonObj.length; i++) {
+        for (let i = 0; i < jsonObj.length; i++) {
+            // Get word translation
             const sentences = [];
             const result = await translator.translateText(jsonObj[i].german, 'de', 'en-US');
             const englishTranslation = result.text;
             
+            // Get sentence translations
             for (const [key, value] of Object.entries(jsonObj[i])) {
                 if (key == "german" || key == "german_full" || !value) continue;
                 try {
@@ -30,9 +32,30 @@ csv()
                 }
             }
 
-            stream.write(`${ jsonObj[i].german.includes(',') ? '"' : ''}${jsonObj[i].german}${ jsonObj[i].german.includes(',') ? '"' : ''},${ englishTranslation.includes(',') ? '"' : ''}${englishTranslation.toLowerCase()}${ englishTranslation.includes(',') ? '"' : ''},${ jsonObj[i].german_full.includes(',') ? '"' : ''}${jsonObj[i].german_full}${ jsonObj[i].german_full.includes(',') ? '"' : ''},`);
+            // Write the whole line into the file
+            stream.write(
+                `${ jsonObj[i].german.includes(',') ? '"' : ''}` + 
+                `${jsonObj[i].german}` + 
+                `${ jsonObj[i].german.includes(',') ? '"' : ''},`
+                +
+                `${ englishTranslation.includes(',') ? '"' : ''}` + 
+                `${englishTranslation.toLowerCase()}` + 
+                `${ englishTranslation.includes(',') ? '"' : ''},`
+                +
+                `${ jsonObj[i].german_full.includes(',') ? '"' : ''}` + 
+                `${jsonObj[i].german_full}` + 
+                `${ jsonObj[i].german_full.includes(',') ? '"' : ''},`);
+            
             for (let j = 0; j < sentences.length; j++) {
-                stream.write(`${sentences[j][0].includes(',') ? '"' : ''}${sentences[j][0]}${sentences[j][0].includes(',') ? '"' : ''},${sentences[j][1].includes(',') ? '"' : ''}${sentences[j][1]}${sentences[j][1].includes(',') ? '"' : ''}${j + 1 == sentences.length ? '\n' : ','}`);
+                stream.write(
+                    `${sentences[j][0].includes(',') ? '"' : ''}` + 
+                    `${sentences[j][0]}` + 
+                    `${sentences[j][0].includes(',') ? '"' : ''},`
+                    +
+                    `${sentences[j][1].includes(',') ? '"' : ''}` + 
+                    `${sentences[j][1]}` + 
+                    `${sentences[j][1].includes(',') ? '"' : ''}` + 
+                    `${j + 1 == sentences.length ? '\n' : ','}`);
             }
         }
     }
